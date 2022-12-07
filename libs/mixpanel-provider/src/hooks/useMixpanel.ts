76a50e55dc3cd1a32b3ebe1bf6d56e0a42716be5
Rene Bonilla <rbonilla@assanet.com>
2022-12-06 22:06:09 -0500

    adding type to hook

--- libs/mixpanel-provider/src/hooks/useMixpanel.ts
+++ libs/mixpanel-provider/src/hooks/useMixpanel.ts
@@ -1,10 +1,11 @@
+import { Mixpanel } from 'mixpanel-browser';
 import React from 'react';
 import {
   MixpanelContextProps,
   MixpanelContext,
 } from '../context/MixpanelProvider';
 
-export const useMixpanel = (idUser?: string) => {
+export const useMixpanel = (idUser?: string): Mixpanel => {
   const { analytics }: MixpanelContextProps = React.useContext(MixpanelContext);
   if (idUser) {
     analytics.identify(idUser);
